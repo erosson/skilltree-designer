d9ad6e8dbd3da99d002add4df3751be46b4a2701
Evan Rosson <github@erosson.org>
2022-09-12 14:57:50 +0000

    tests and watcher

---
+++ generator/tests/model.test.ts
@@ -0,0 +1,15 @@
+import { describe, expect, test } from '@jest/globals'
+import * as M from '../src/model'
+
+test('jest typechecks', () => {
+    const x: number = "three"
+})
+test('create a node', () => {
+    const n = M.node()
+    expect(n.type).toEqual(M.ElementType.Node)
+})
+test('create an edge offset', () => {
+    const n = M.node()
+    const m = M.node()
+    // const es = M.edges([n, m], M.edge(n.id, m.id))
+})
